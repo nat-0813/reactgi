f39dc80bcb908025414169d117208a03c1061ce9
nat-0813 <129187214+nat-0813@users.noreply.github.com>
2023-07-07 11:25:44 -0400

    first commit
    made changes to index.js file

--- reactgi/src/index.js
+++ reactgi/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+class BasicInfo extends React.Component {
+  render() {
+    const { name, birthday, phoneNumber } = this.props.person;
+    return (
+      <div>
+        <h1>Basic Information</h1>
+        <p>Name: {name}</p>
+        <p>Birthday: {birthday}</p>
+        <p>Phone Number: {phoneNumber}</p>
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: [
+        {
+          name: "Natalie Aguilar-Hernandez",
+          birthday: "11/15/1996",
+          phoneNumber: "123-456-7890",
+        },
+        {
+          name: "Ryan Reynolds",
+          birthday: "10/23/1976",
+          phoneNumber: "444-555-6666",
+        },
+        {
+          name: "Drew Barrymore",
+          birthday: "2/22/1975",
+          phoneNumber: "555-777-2222",
+        },
+      ],
+    };
+  }
+  render() {
+    const peopleList = this.state.person.map((person, index) => (
+      <BasicInfo key={index} person={person} />
+    ));
+    return <div>{peopleList}</div>;
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
